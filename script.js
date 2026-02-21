222451f36ab3ecb8da47605a4226e94b3ac9f26b
sohal <sohalchunarkar8@gmail.com>
2026-02-22 00:12:30 +0530

    solved the assignment

--- script.js
+++ script.js
@@ -6,22 +6,40 @@ let arr = [
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
+
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  arr.map((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
-  //Write your code here, just console.log
+  arr.push({ id: 4, name: "susan", age: "20", profession: "intern" });
+  console.log(arr);
 }
 
 function removeAdmin() {
-  //Write your code here, just console.log
+  arr = arr.filter((employee) => employee.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  let newArr = [
+    { id: 5, name: "mike", age: "22", profession: "tester" },
+    { id: 6, name: "anna", age: "21", profession: "designer" },
+    { id: 7, name: "tom", age: "23", profession: "manager" },
+  ];
+
+  let combinedArray = arr.concat(newArr);
+  console.log(combinedArray);
 }
